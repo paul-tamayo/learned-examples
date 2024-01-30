836111ae1cd77374941de8ca5c9a09be3bc76b29
Paul Tamayo <dpaul.tamayo@yahoo.com>
2024-01-29 19:30:51 -0500

    * Commit de respaldo

--- mockito/src/test/java/com/paultamayo/mockito/service/PersonServiceTest.java
+++ mockito/src/test/java/com/paultamayo/mockito/service/PersonServiceTest.java
@@ -2,7 +2,6 @@ package com.paultamayo.mockito.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
