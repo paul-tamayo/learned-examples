1d563e237979006586c648212adcd29ad51fe96f
Diego Tamayo <dpaul.tamayo@yahoo.com>
2024-04-11 09:09:28 -0500

    * Commit de respaldo

---
+++ spring/springsecurity6/spring-boot-security-02/src/main/java/com/paultamayo/demo/security/controller/BalanceController.java
@@ -0,0 +1,18 @@
+package com.paultamayo.demo.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "Home";
+	}
+
+	@GetMapping("/my-balance")
+	public String getBalanceDetails() {
+		return "Balance a detalle...";
+	}
+}
