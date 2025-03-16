5d38361f2013697c4b78fb0f3def7263f32ef6fa
Paul Tamayo <dpaul.tamayo@yahoo.com>
2025-03-16 13:17:38 -0500

    * Commit de respaldo

--- java-examples/design-pattern/src/main/java/com/paultamayo/patterndesign/creational/_01_builder/ComputeBuilderWithLombok.java
+++ java-examples/design-pattern/src/main/java/com/paultamayo/patterndesign/creational/_01_builder/ComputeBuilderWithLombok.java
@@ -1,20 +1,10 @@
 package com.paultamayo.patterndesign.creational._01_builder;
 
 import lombok.Builder;
-import lombok.Data;
 
 public interface ComputeBuilderWithLombok {
 
     @Builder
-    @Data
-    public static class Computer {
-
-        private String cpu;
-
-        private String ram;
-
-        private String storage;
-
-        private String gpu;
+    public record Computer(String cpu, String ram, String storage, String gpu) {
     }
 }
